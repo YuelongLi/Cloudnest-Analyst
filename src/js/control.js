6d6f6676078016db28ae102072ebaf0a7a3652af
YuelongLi <cloudnest.liyuelong@gmail.com>
2017-12-22 14:51:26 -0500

    Update infrastructure

--- src/js/control.js
+++ src/js/control.js
@@ -5,17 +5,25 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 5;
+var parametric = function(u,v){
+  u = 2*u*Math.PI-Math.PI;
+  v = 2*v*Math.PI-Math.PI;
+  return new THREE.Vector3(Math.cos(u)*Math.cos(v),Math.sin(u)*Math.sin(v),Math.sin(u));
+};
 
+var material = new THREE.MeshNormalMaterial();
+var geometry = new THREE.ParametricBufferGeometry(parametric, 50, 50);
+
+var cube = new THREE.Mesh(geometry, material);
+camera.position.z = 5;
+scene.add(cube);
+var t = 0;
 var render = function () {
-  requestAnimationFrame( render );
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  t += 0.01;
+  cube.rotation.y = t;
+  cube.rotation.x = 0;
   renderer.render( scene, camera );
+  requestAnimationFrame( render );
 };
 
 render();
